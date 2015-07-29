f3419bd485b616e1b4bd0628903ed4599777e790
Evan Dekhayser <erdekhayser@gmail.com>
2015-07-29 14:39:40 -0400

    This is probably as good as it's gonna get :]

--- DoubleLoop.swift
+++ DoubleLoop.swift
@@ -6,38 +6,42 @@
 //
 //
 
-class DoubleLoop<T where T: Comparable, T: ForwardIndexType>: SequenceType{
+struct DoubleLoop<T: ForwardIndexType>: SequenceType {
+	let outerRange: Range<T>
+	let innerRange: Range<T>
 	
-	var firstRange: Range<T>
-	var secondRange: Range<T>
-	let copySecondRange: Range<T>
-	
-	init(r1: Range<T>, r2: Range<T>){
-		firstRange = r1
-		secondRange = r2
-		copySecondRange = r2
+	init(_ outerRange: Range<T>, _ innerRange: Range<T>) {
+		self.outerRange = outerRange
+		self.innerRange = innerRange
 	}
 	
 	typealias Generator = AnyGenerator<(T,T)>
 	
 	func generate() -> Generator {
+		var x = self.outerRange.startIndex
+		var y = self.innerRange.startIndex
 		
 		return anyGenerator {
-			if self.secondRange.startIndex >= self.secondRange.endIndex{
-				self.firstRange.startIndex++
-				self.secondRange = self.copySecondRange
-				if self.firstRange.startIndex >= self.firstRange.endIndex{
-					return nil
-				}
+			if x == self.outerRange.endIndex {
+				return nil
 			}
-			return (self.firstRange.startIndex, self.secondRange.startIndex++)
+			
+			let result = (x, y)
+			
+			++y
+			if y == self.innerRange.endIndex {
+				y = self.innerRange.startIndex
+				++x
+			}
+			
+			return result
 		}
 	}
-	
 }
+
 
 infix operator ∫ { associativity left precedence 134 }
 
 func ∫ <T: ForwardIndexType>(left: Range<T>, right: Range<T>) -> DoubleLoop<T>{
-	return DoubleLoop(r1: left, r2: right)
+	return DoubleLoop(left, right)
 }
